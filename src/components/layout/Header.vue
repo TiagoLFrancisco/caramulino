fd87fe9ed6a0bac1fdb65f80ba8898087e8ea66a
TiagoLFrancisco <tiagolopesfrancisco@gmail.com>
2025-06-04 23:30:10 +0100

    First commit: remove scaffold trash

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="navbar-container">
+    <div class="logo">Area</div>
+    <span class="material-icons-round" @click="menuClosed = false" v-show="menuClosed">menu</span>
+    <span class="material-icons-round" @click="menuClosed = true" v-show="!menuClosed">close</span>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const menuClosed = ref(true)
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<template>
+  <main>
+    <div>hello world</div>
+  </main>
+</template>
+
+<script setup></script>
